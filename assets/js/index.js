40d53ba544a64d5598bf0098a4e5d11469d67acc
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-09-17 00:08:35 +0530

    update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,19 @@
 function playAudio(letter){
     var audio;
     switch(letter){
-        case 'w' :  audio = new Audio("./sounds/tom-1.mp3");
+        case 'w' :  audio = new Audio("./assets/sounds/tom-1.mp3");
                     break;
-        case 'a' :  audio = new Audio("./sounds/tom-2.mp3");
+        case 'a' :  audio = new Audio("./assets/sounds/tom-2.mp3");
                     break;
-        case 's' :  audio = new Audio("./sounds/tom-3.mp3");
+        case 's' :  audio = new Audio("./assets/sounds/tom-3.mp3");
                     break;
-        case 'd' :  audio = new Audio("./sounds/tom-4.mp3");
+        case 'd' :  audio = new Audio("./assets/sounds/tom-4.mp3");
                     break;
-        case 'j' :  audio = new Audio("./sounds/crash.mp3");
+        case 'j' :  audio = new Audio("./assets/sounds/crash.mp3");
                     break;
-        case 'k' :  audio = new Audio("./sounds/kick-bass.mp3");
+        case 'k' :  audio = new Audio("./assets/sounds/kick-bass.mp3");
                     break;
-        case 'l' :  audio = new Audio("./sounds/snare.mp3");
+        case 'l' :  audio = new Audio("./assets/sounds/snare.mp3");
                     break;
         default :   console.log("invalid");
     }
